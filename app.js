047951dd90a904f3d3c74ef671c2eb6312329148
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-04-27 23:48:32 +0600

    show page is done for now

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express'),
 mongoose = require('mongoose'),
 songRoute = require('./routes/song'),
+methodOverride = require('method-override'),
  app   = express();
  //mongoose config
  mongoose.connect('mongodb://localhost/coolsongs', {
@@ -14,6 +15,7 @@ songRoute = require('./routes/song'),
  app.set('view engine', 'ejs');
  app.use(express.static('public'));
  app.use(express.urlencoded({extended:true}))
+ app.use(methodOverride('_method'))
 //routes
  app.get('/', (req, res)=>{
      res.render('home');
--- routes/song.js
+++ routes/song.js
@@ -32,5 +32,62 @@ Song.create(input, (err, done)=>{
         res.redirect('/songs');
     }
 })
-})
+});
+//show page
+router.get('/songs/:id', (req, res)=>{
+    Song.findById(req.params.id, (err, song)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.render('song/show', {song : song})
+        }
+    })
+});
+//edit
+router.get('/songs/:id/edit', (req, res)=>{
+    Song.findById(req.params.id, (err, song)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.render('song/edit', {song : song})
+        }
+    })
+});
+router.put('/songs/:id', (req, res)=>{
+    const input = req.body.song;
+function yt(){
+    if(input.yt.length > 40){
+      input.yt = input.yt.slice(32)
+    } else if( input.yt.length<30){
+      input.yt = input.yt.slice(17)
+    }
+  } ;
+  yt();
+    Song.findByIdAndUpdate(req.params.id, input, (err, done)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.redirect('/songs/' + req.params.id);
+        }
+    } )
+});
+//delete
+router.get('/songs/:id/delete', (req, res)=>{
+    Song.findById(req.params.id, (err, song)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.render('song/delete', {song : song})
+        }
+    })
+});
+router.delete('/songs/:id', (req, res)=>{
+    Song.findByIdAndDelete(req.params.id, (err, done)=>{
+        if(err){
+            console.log(err)
+        } else{
+            res.redirect('/songs');
+        }
+    })
+});
 module.exports = router;
