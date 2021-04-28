c54b0ed9d3b1e2b1723b23f388a3ad96667ce978
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-04-28 14:53:46 +0600

    working on comments

---
+++ routes/comment.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Song = require('../models/song');
+const Comment = require('../models/comment');
+
+router.post('/songs/:id/comment', (req, res)=>{
+
+})
+
+module.exports = router;
