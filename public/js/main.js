2c3e1fa06503c07504556db134bc71b3732a6040
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-05-07 23:08:24 +0600

    search bar needs desing

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,11 @@ const loader = document.querySelector('.loader');
 window.addEventListener('load', ()=>{
 loader.classList.add('off');
 })
+
+
+const btn = document.querySelector('.icon');
+const form = document.querySelector('.search');
+
+btn.addEventListener('click', ()=>{
+    form.classList.toggle('off');
+})
