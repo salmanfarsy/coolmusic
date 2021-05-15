d2d5cd102f467ef97c549ea8a83779d75c7ff3f9
salmanfarsy <75604210+salmanfarsy@users.noreply.github.com>
2021-05-15 22:03:43 +0600

    i think slider is done

--- public/js/slide.js
+++ public/js/slide.js
@@ -2,22 +2,33 @@
 
 const img = document.querySelectorAll('.image');
 const topImage = document.querySelector('img');
+const div = document.querySelector('.slide');
+console.log(div);
 let count = 0;
 // img.forEach((x)=>{
 //     console.log(x.src);
 // });
 
+// function slide(){
+//     if(img.length>0){
+//         topImage.src = img[count].src;
+//         topImage.classList.toggle('animation')
+//         topImage.classList.toggle('animation0')
+//        console.log(img[count]);
+//        count++;
+//        check();
+//     }
+// }
 function slide(){
-    if(img.length>0){
-        topImage.src = img[count].src;
-        topImage.classList.toggle('animation')
-        topImage.classList.toggle('animation0')
-       console.log(img[count]);
+  
+      div.style.background = `url(${img[count].src}) `;
+      div.style.backgroundSize ='cover' ;
+      
+
+   
+     
        count++;
        check();
-    }
-
-
 
 }
 
@@ -28,5 +39,11 @@ function check(){
     
     
 }
-window.addEventListener('load', setInterval(slide, 10000));
+ setInterval(slide, 5000);
+ function set(){
+    div.style.background = `url(${img[0].src}) `;
+    div.style.backgroundSize ='cover' ;
+    div.style.backgroundAttachment ='fixed';
+ };
+ set();
 
